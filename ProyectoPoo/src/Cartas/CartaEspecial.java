eb0c93d3997da115475ab08a1a9bd75962392f08
gabiserrano <00090322@uca.edu.sv>
2024-06-03 18:04:09 -0600

    First parts of the Project

---
+++ ProyectoPoo/src/Cartas/CartaEspecial.java
@@ -0,0 +1,10 @@
+package Cartas;
+
+public class CartaEspecial extends Carta{
+    private String efecto;
+
+    public CartaEspecial(String nombre, String descripcion) {
+        super(nombre, descripcion);
+    }
+}
+
---
+++ ProyectoPoo/src/Cartas/CartaNormal.java
@@ -0,0 +1,9 @@
+package Cartas;
+
+public class CartaNormal extends Carta {
+    private String efecto;
+
+    public CartaNormal(String nombre, String descripcion) {
+        super(nombre, descripcion);
+    }
+}
